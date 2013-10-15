755153f3ac637dbe8fe128c6eeb70f2910356426
Cyril "Kyriog" Glapa <cyril.glapa@keuse.fr>
2013-10-15 02:41:22 +0200

    Declaring /unsign command

--- src/fr/kyriog/bukkit/signtools/SignToolsPlugin.java
+++ src/fr/kyriog/bukkit/signtools/SignToolsPlugin.java
@@ -4,11 +4,15 @@ import org.bukkit.World;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.kyriog.bukkit.signtools.commands.UnsignCommand;
+
 public class SignToolsPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		PluginManager pm = getServer().getPluginManager();
 		World defaultWorld = getServer().getWorlds().get(0);
 		pm.registerEvents(new SignToolsListener(getLogger(), defaultWorld), this);
+
+		getCommand("unsign").setExecutor(new UnsignCommand());
 	}
 }
---
+++ src/fr/kyriog/bukkit/signtools/commands/UnsignCommand.java
@@ -0,0 +1,12 @@
+package fr.kyriog.bukkit.signtools.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class UnsignCommand implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+}
