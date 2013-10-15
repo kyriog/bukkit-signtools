94a4d9e44c5c33522fe902c4ff2afc56680b23e5
Cyril "Kyriog" Glapa <cyril.glapa@keuse.fr>
2013-10-15 02:28:12 +0200

    Declaring listener

---
+++ src/fr/kyriog/bukkit/signtools/SignToolsListener.java
@@ -0,0 +1,7 @@
+package fr.kyriog.bukkit.signtools;
+
+import org.bukkit.event.Listener;
+
+public class SignToolsListener implements Listener {
+
+}
--- src/fr/kyriog/bukkit/signtools/SignToolsPlugin.java
+++ src/fr/kyriog/bukkit/signtools/SignToolsPlugin.java
@@ -1,7 +1,12 @@
 package fr.kyriog.bukkit.signtools;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SignToolsPlugin extends JavaPlugin {
-
+	@Override
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new SignToolsListener(), this);
+	}
 }
